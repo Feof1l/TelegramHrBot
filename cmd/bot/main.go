c84ef5c031d525481f434cdbc1b9ae6638b8c670
feof1l <ivan.vasilov.03@mail.ru>
2024-03-05 11:34:38 +0300

    echo bot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const token = "7108620322:AAEfFMb5iMADlFXs0c_QMCmc9BhD8hwFA8U"
 
 func main() {
-	fmt.Println("Hello")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Feof1l/TelegramHrBot
 
 go 1.20
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
