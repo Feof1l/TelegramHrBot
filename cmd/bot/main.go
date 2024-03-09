b92766ba48bc78a28f2705b0da9d7d77b0fe831e
feof1l <ivan.vasilov.03@mail.ru>
2024-03-09 14:22:59 +0300

    add some buttons

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/Feof1l/TelegramHrBot/pkg/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-const token = "7108620322:AAEfFMb5iMADlFXs0c_QMCmc9BhD8hwFA8U"
-
 func main() {
+
+	// создаем новый логер для вывода информационных сообщенйи в поток stdout c припиской info
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	//аналогично для логов с ошибками, такеж включим вывод фйла и номера  строки, где произошла ошибка
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	/*app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}*/
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Panic(err)
+		errorLog.Panic(err)
 	}
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	infoLog.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -24,13 +34,37 @@ func main() {
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		// универсальный ответ на любое сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		reply := ""
+		if update.Message == nil { // If we got a message
+			//infoLog.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			continue
 		}
+
+		switch update.Message.Command() {
+		case "start":
+			reply = telegram.StartMessage
+			msg.ReplyMarkup = telegram.AnswerKeyBoard
+		}
+		switch update.Message.Text {
+		case "open":
+			//msg.ReplyMarkup = numericKeyboard
+		case "close":
+			//msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+
+		}
+		// создаем ответное сообщение
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+
+		if _, err := bot.Send(msg); err != nil {
+			errorLog.Panic(err)
+		}
+
 	}
+}
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -1,1 +1,26 @@
 package telegram
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+
+var StartMessage = "Привет! Я - HR бот.Сейчас задам тебе несколько вопросов. Если готов(а) продолжить общение со мной, жми `ДА`. \nЕсли выберешь `ДА`, соглашаешься с условиями обрабтки данных.Продолжим диалог? "
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1"),
+		tgbotapi.NewKeyboardButton("2"),
+		tgbotapi.NewKeyboardButton("3"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4"),
+		tgbotapi.NewKeyboardButton("5"),
+		tgbotapi.NewKeyboardButton("6"),
+	),
+)
+
+var AnswerKeyBoard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Да"),
+		tgbotapi.NewKeyboardButton("Нет"),
+		tgbotapi.NewKeyboardButton("Заблокировать"),
+	),
+)
